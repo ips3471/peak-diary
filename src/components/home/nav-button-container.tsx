00c644e0d4cd02d38c6c3ada002b224b11c24444
han <ips3471@gmail.com>
2023-04-26 12:44:43 +0900

    home item style

--- src/components/home/nav-button-container.tsx
+++ src/components/home/nav-button-container.tsx
@@ -18,7 +18,7 @@ export default function NavButtonContainer({
 					return window.alert('로그인이 필요한 서비스입니다');
 				}
 			}}
-			className={`col-span-2 h-2/6 rounded-xl border-2 shadow-sm ${backgroundTailwindStyle} bg-cover bg-no-repeat bg-center`}
+			className={`col-span-2 h-3/6 rounded-xl border-2 shadow-sm ${backgroundTailwindStyle} bg-cover bg-no-repeat bg-center`}
 		>
 			{children}
 		</section>
